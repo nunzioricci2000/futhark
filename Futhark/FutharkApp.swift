69e13c60aa9f5609235614d1a4fd0897625356dd
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2023-02-27 00:11:05 +0100

    Project Created

---
+++ Futhark/FutharkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutharkApp.swift
+//  Futhark
+//
+//  Created by Nunzio Ricci on 27/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FutharkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
