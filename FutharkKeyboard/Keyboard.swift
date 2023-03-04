14227a0d4a7e580fe135c912e8dcca302b395a43
Nunzio Ricci <nunzio.ricci.2000@gmail.com>
2023-03-04 04:29:50 +0100

    Update Keyboard.swift
    Create Keyboard Grid

--- FutharkKeyboard/Keyboard.swift
+++ FutharkKeyboard/Keyboard.swift
@@ -9,7 +9,34 @@ import SwiftUI
 
 struct Keyboard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Grid(horizontalSpacing: 6, verticalSpacing: 12) {
+            GridRow {
+                Color.white
+                Color.white
+                Color.white
+                Color.white
+            }
+            GridRow {
+                Color.white
+                Color.white
+                Color.white
+                Color.white
+            }
+            GridRow {
+                Color.white
+                Color.white
+                Color.white
+                Color.white
+            }
+            GridRow {
+                Color.white
+                Color.white
+                Color.white
+                Color.white
+            }
+        }
+        .padding(.vertical, 8)
+        .padding(.horizontal, 3)
     }
 }
 
